ef4a5f3240e440b80d64087889f4815abfb9f5cb
Renato Pradebon <renatopradebon@hotmail.com>
2016-04-28 00:10:35 -0300

    Finalizando classe excecao2

--- Linguagem de Programação - JAVA 1/Módulo 5/Excecao/src/Excecao2.java
+++ Linguagem de Programação - JAVA 1/Módulo 5/Excecao/src/Excecao2.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,12 +13,15 @@ public class Excecao2 {
 
         try {
             s = memoria.readLine();
+            int numero = Integer.parseInt(s);
+            System.out.println("\n O quadrado do valor de entrada eh: " + numero * numero);
         } catch (IOException erro1) {
             System.out.println("\nErro de entrada de dados: " + erro1);
+        } catch (NumberFormatException erro2) {
+            System.err.println("\n Deve entrar com um número: erro2--> " + erro2);
         } finally {
             System.out.println("\n Entrou no Finally");
         }
-        int numero = Integer.parseInt(s);
-        System.out.println("\n O quadrado do valor de entrada eh: "+ numero*numero) ;
+        System.out.println("\n O valor de entrada foi " + s);
     }
 }
