dfe6a2084e1cb809a903b44b02e048d24602b5d8
Renato Pradebon <renatopradebon@hotmail.com>
2016-05-31 22:36:38 -0300

    Exercicio da semana do dia 23 a 29 de maio

--- Banco de Dados/JPA2-Hibernate/src/jpa2/hibernate/ListaEditorasComJPA.java
+++ Banco de Dados/JPA2-Hibernate/src/jpa2/hibernate/ListaEditorasComJPA.java
@@ -13,8 +13,11 @@ public class ListaEditorasComJPA {
 		// TODO Auto-generated method stub
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria");
 		EntityManager manager = factory.createEntityManager();
+		
 		Query query = manager.createQuery("SELECT e FROM Editora e");
+		
 		List<Editora> editoras = query.getResultList();
+		
 		for (Editora e : editoras) {
 			System.out.println("EDITORA: " + e.getNome() + " - " + e.getEmail());
 		}
---
+++ Banco de Dados/JPQL/src/JPQL/TesteParametros.java
@@ -0,0 +1,31 @@
+package JPQL;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class TesteParametros {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpql");
+		EntityManager manager = factory.createEntityManager();
+
+		Query query = manager.createNamedQuery("Livro.findByPrecoMinimo");
+		Double precoMinimoLivro = 18.00;
+
+		query.setParameter("preco", precoMinimoLivro);
+
+		List<Livro> livros = query.getResultList();
+
+		for (Livro livro : livros) {
+			System.out.println("Nome: " + livro.getNome());
+			System.out.println("Preço: " + livro.getPreco());
+		}
+
+	}
+
+}
